6bc824a0f05455ba84a3d8b51845977e7cfda48b
imperative-source-6800 <tim.faniran@gmail.com>
2019-05-05 15:07:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
-arr = []
- arr.each {|i| puts i**2}
+arr = [1,2,3]
+ arr.each {|i| print i**2}
 end
 
 numbers = [1,2,3]
@@ -9,3 +9,18 @@ square_array(numbers)
 
 numbers_ = [9,10,16,25]
 square_array(numbers)
+
+# #!/usr/bin/ruby
+# def square_array(array)
+# #   # your code here
+# arr = [1,2,3]
+# arr.each {|i| puts i**2}
+# end
+
+# numbers = [1,2,3]
+# square_array(numbers)
+
+# numbers_ = [9,10,16,25]
+# square_array(numbers)
+# numbers__ = [9,10,16,25]
+# square_array(numbers)
