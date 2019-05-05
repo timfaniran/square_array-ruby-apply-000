5d1a8e10a86804e17b4f3217b9528eac47f7edc0
imperative-source-6800 <tim.faniran@gmail.com>
2019-05-05 15:11:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,9 +7,9 @@ arr = [1,2,3]
  a.each {|i| puts i**2}
 end
 
-numbers = [1,2,3]
-square_array(numbers)
+# numbers = [1,2,3]
+# square_array(numbers)
 
-numbers_ = [9,10,16,25]
-square_array(numbers)
+# numbers_ = [9,10,16,25]
+# square_array(numbers)
 
